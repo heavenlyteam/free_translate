97dbb89452d172cb9349fbbec34e5cb59cd38c6c
Vladislav Len <vladislav.len@constructor-of-innovations.com>
2016-12-19 23:36:52 +0200

    Translate engine work

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/translator');
 var Schema = mongoose.Schema;
@@ -16,6 +17,7 @@ var users = require('./routes/users');
 var translate = require('./routes/translate');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,16 +34,69 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
+app.get('/create_key', function (req, res) {
+
+    keyCollection.create({
+        owner_id: 1027,
+        key: 'vfhbyjxrf123'
+    }, function (err, doc) {
+
+        if(!err && doc) {
+            res.send(doc);
+        }else {
+            res.send({
+                status: false
+            })
+        }
+
+    })
+
+});
+
 app.post('/translate', function (req, res, next) {
-    var key = req.param.key;
-    var from = req.param.from;
-    var to = req.param.to;
-    var text = req.param.to;
+    var key = req.param('key');
+    var from = req.param('from');
+    var to = req.param('to');
+    var text = req.param('text');
 
     if (key && from && to && text) {
-        res.send({
-            status: true
+
+        keyCollection.findOne({
+            key: key
+        }, function(err, doc) {
+
+            if(err) {
+                res.send({
+                    status: false,
+                    desc: 'Some database error'
+                });
+            }
+
+            if (!err && doc && doc.key == key) {
+
+                request({url: 'https://www.googleapis.com/language/translate/v2', qs: {
+                    'key': 'AIzaSyBNl6H2VeDwBUoAYpP6B1eqm9XmgGwlp_s',
+                    'source': from,
+                    'target': to,
+                    'q': text
+                }}, function (err, response, body) {
+                    if(err) { res.send({status: false}) }
+                    var jsonBody = JSON.parse(body);
+
+                    res.send({
+                        status: true,
+                        translated: jsonBody.data.translations[0].translatedText
+                    });
+                })
+            }else {
+                res.send({
+                    status: false,
+                    desc: 'Key error'
+                })
+            }
         });
+
     }else {
 
         res.send({
